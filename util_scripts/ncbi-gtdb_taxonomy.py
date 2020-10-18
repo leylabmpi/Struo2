2ff1fdc7cf636ed8a11e95cc5e98045ee1220fee
Nicholas Youngblut <nicholas.youngblut@tuebingen.mpg.de>
2020-10-18 13:03:14 +0200

    multiprocessing for ncbi-gtdb

--- util_scripts/ncbi-gtdb_taxonomy.py
+++ util_scripts/ncbi-gtdb_taxonomy.py
@@ -6,9 +6,12 @@ import sys
 import re
 import argparse
 import logging
+import random
 import csv
 import urllib.request
 import codecs
+import functools
+import multiprocessing as mp
 from collections import Counter
 # 3rd party
 import networkx as nx
@@ -34,8 +37,15 @@ parser.add_argument('-q', '--query-taxonomy', type=str, default='ncbi_taxonomy',
                     help='Taxonomy of the query list (Default: %(default)s)')
 parser.add_argument('-f', '--fraction', type=float, default=0.9,
                     help='Homogeneity of LCA (fraction) in order to be used (Default: %(default)s)')
+parser.add_argument('-m', '--max-tips', type=int, default=100,
+                    help='Max no. of tips used for LCA determination. If more, subsampling w/out replacement (Default: %(default)s)')
+#parser.add_argument('-M', '--max-tax', type=str, default='family',
+#                    help='Coursest taxonomic level of query that is allowed (Default: %(default)s)')
+parser.add_argument('-p', '--procs', type=int, default=1,
+                    help='No. of parallel processes (Default: %(default)s)')
+parser.add_argument('-v', '--verbose', action='store_true', default=False,
+                    help='Verbose output (Default: %(default)s)')
 parser.add_argument('--version', action='version', version='0.0.1')
-
 logging.basicConfig(format='%(asctime)s - %(message)s', level=logging.DEBUG)
 
 
@@ -51,8 +61,8 @@ def add_taxonomy(line, line_num, header, G, tax='ncbi_taxonomy'):
     T = line[header[tax]].split(';')
     T = [regex.sub('', x) for x in T]
     if len(T) < 7:
-        msg = 'WARNING: Line{}: "{}" length is <7'
-        logging.info(msg.format(line_num, tax))
+        msg = 'WARNING: [Line={}, Acc={}]: "{}" length is <7\n'
+        sys.stderr.write(msg.format(line_num, acc, tax))
         for i in range(7):
             try:
                 _ = T[i]
@@ -98,6 +108,7 @@ def load_gtdb_metadata(infile, G):
         # taxonomies
         add_taxonomy(line, i, header, G, tax='gtdb_taxonomy')
         add_taxonomy(line, i, header, G, tax='ncbi_taxonomy')
+    # closing
     try:
         inF.close()
     except AttributeError:
@@ -112,11 +123,22 @@ def DiGraph_w_root():
     G.add_node('root')
     return G
 
-
 def lca_frac_pass(D, lca_frac):
+    """
+    Determine which, if any, of the LCAs pass the homogeneity cutoff.
+    "homogeneity" means the fraction of tips with the target LCA 
+      (eg., 90% of all tips have this LCA).
+    If the cutoff is not passed, then returning [None,None]
+    """
     D = Counter(D)
-    mc = D.most_common(1)
-    frac = mc[0][1] / float(sum(D.values()))
+    try:
+        mc = D.most_common(1)
+    except IndexError:
+        return [None,None]
+    try:
+        frac = mc[0][1] / float(sum(D.values()))
+    except IndexError:
+        return [None,None]
     if frac >= lca_frac:
         return [mc[0][0], str(round(frac, 3))]
     else:
@@ -141,53 +163,88 @@ def lca_many_nodes(G, nodes, lca_frac=1.0):
     for i in range(len(T))[::-1]:
         lca = lca_frac_pass(T[i], lca_frac)
         if lca[0] is not None:
-            return lca
+            return lca + [hierarchy[i]]
     raise ValueError('Cannot find LCA for nodes: {}'.format(','.join(nodes)))
 
-def query_tax(tax_queries, G, qtax, ttax, lca_frac=1.0):
-    logging.info('Loading queries: {}'.format(tax_queries))
+def _query_tax(tax_queries, G, qtax, ttax, lca_frac=1.0, max_tips=100, verbose=False):
+    """
+    Querying list of taxonomic names.
+    """
+    pid = os.getpid()
     idx = {}
     status = {'hit' : 0, 'no hit': 0}
     # iterating queries
-    with open(tax_queries) as inF:
-        for line in inF:
-            line = line.rstrip()
-            if line == '' or line == 'root':
-                continue
-            # is node in G[qtax]?
-            tips = []
-            try:
-                # getting descendents of the node
-                for desc in list(descendants(G[qtax], line)):
-                    if G[qtax].nodes[desc]['taxonomy'] == 'strain':
-                        tips.append(desc)
-                status['hit'] += 1
-            except nx.exception.NetworkXError:
-                status['no hit'] += 1
-            # if tips, getting LCA in target-taxonomy
-            if len(tips) > 0:                
-                LCA = lca_many_nodes(G[ttax], tips, lca_frac=lca_frac)
-                idx[line] = LCA
-            # status
-            x = status['hit'] + status['no hit']
-            if x % 10 == 0:
-                #sys.stderr.write('Queries processed: {}\r'.format(x))
-                logging.info('Queries processed: {}'.format(x))
-                                
+    for Q in tax_queries:
+        tips = []
+        try:
+            # getting descendents of the node
+            tips = [desc for desc in descendants(G[qtax], Q) if \
+                    G[qtax].nodes[desc]['taxonomy'] == 'strain']
+            status['hit'] += 1
+        except nx.exception.NetworkXError:
+            status['no hit'] += 1
+        # if tips, getting LCA in target-taxonomy
+        n_tips = len(tips)
+        if n_tips > 0:
+            if n_tips > max_tips:                    
+                tips = random.sample(tips, k=max_tips)
+            LCA = lca_many_nodes(G[ttax], tips, lca_frac=lca_frac)
+            idx[Q] = LCA
+        else:
+            idx[Q] = ['unclassified', 'NA', 'NA']
+        # status
+        x = status['hit'] + status['no hit']
+        if verbose and x % 1000 == 0:
+            frac = round(float(x) / len(tax_queries) * 100, 2)
+            logging.info('PID{}: Queries processed: {} ({}%)'.format(pid, x, frac))       
     # status
-    msg = 'No. of hits to {}: {}'
-    logging.info(msg.format(qtax, status['hit']))
-    msg = 'No. lacking hits to {}: {}'
-    logging.info(msg.format(qtax, status['no hit']))
+    msg = 'PID{}: Finished! Queries={}, Hits={}, No-Hits={}'
+    logging.info(msg.format(pid, status['hit'] + status['no hit'],
+                            status['hit'], status['no hit']))
     # return
     return idx
 
-def write_table(idx, qtax, ttax):
-    print('\t'.join([qtax, ttax, 'lca_frac']))
-    for k,v in idx.items():
-        print('\t'.join([k] + v))
+def query_tax(tax_queries, G, tax, lca_frac=1.0, max_tips=100, procs=1, verbose=False):
+    ttax = 'ncbi_taxonomy' if tax == 'gtdb_taxonomy' else 'gtdb_taxonomy'
+    # loading & batching queries
+    logging.info('Reading in queries: {}'.format(tax_queries))    
+    q_batch = [[] for i in range(procs)]
+    with open(tax_queries) as inF:
+        for i,line in enumerate(inF):
+            line = line.rstrip()
+            if line == '' or line == 'root':
+                continue
+            q_batch[i % procs].append(line)
+            # debug
+            #if i > 10000:
+            #    break
+    # query graphs
+    logging.info('Querying taxonomies...')
+    func = functools.partial(_query_tax, G=G, qtax=tax, ttax=ttax,
+                             lca_frac = lca_frac, max_tips=max_tips,
+                             verbose=verbose)
+    if procs > 1:
+        P = mp.Pool(procs)
+        idx = P.map(func, q_batch)
+    else:
+        idx = map(func, q_batch)    
+    return idx
+
+def write_table(idx, qtax):
+    """
+    Writing tab-delim table of taxonomy mappings to STDOUT
+    """
+    logging.info('Writing table to STDOUT...')
+    ttax = 'ncbi_taxonomy' if qtax == 'gtdb_taxonomy' else 'gtdb_taxonomy'    
+    print('\t'.join([qtax, ttax, 'lca_frac', 'target_tax_level']))
+    for x in idx:
+        for k,v in x.items():
+            print('\t'.join([k] + v))
 
 def main(args):
+    """
+    Main interface
+    """
     # loading the graphs
     G = {'ncbi_taxonomy' : DiGraph_w_root(),
          'gtdb_taxonomy' : DiGraph_w_root()}
@@ -195,25 +252,16 @@ def main(args):
         logging.info('Loading: {}'.format(F))
         load_gtdb_metadata(F, G)
     # querying
-    ttax = 'ncbi_taxonomy' if args.query_taxonomy == 'gtdb_taxonomy' else 'gtdb_taxonomy'
-    idx = query_tax(args.tax_queries, G, qtax=args.query_taxonomy, ttax=ttax,
-                    lca_frac = args.fraction)
+    idx = query_tax(args.tax_queries, G,
+                    tax=args.query_taxonomy,
+                    lca_frac = args.fraction,
+                    max_tips = args.max_tips,
+                    procs = args.procs,
+                    verbose = args.verbose)
     # writing results
-    write_table(idx, qtax=args.query_taxonomy, ttax=ttax)
+    write_table(idx, qtax=args.query_taxonomy)
              
 if __name__ == '__main__':
     args = parser.parse_args()
     main(args)
 
-# # networkx approach
-# * for each taxon in the GTDB
-#   * create DAG of NCBI taxonomy (taxon at tip, root => tips)
-#   * create DAG of GTDB taxonomy (taxon at tip, root => tips)
-# * For each query:
-#   * find in (NCBI|GTDB) DAG
-#     * G.nodes(taxonomy="foo") 
-#   * find all extant children
-#     * networkx.algorithms.dag.descendants()
-#     * DiGraph.successors(n)  # until no more successor
-#   * for extant children in other db, find LCA
-#     * all_pairs_lowest_common_ancestor()
